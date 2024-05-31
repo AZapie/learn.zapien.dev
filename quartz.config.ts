6deddb9bd2f46213ca32f6a72796be2e8b37792f
Adrian Zapien <Adrian.Zapien@Intralox.com>
2024-05-31 11:11:19 -0500

    Quartz sync: May 31, 2024, 11:11 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Intralox-EMS",
+    pageTitle: "⚙️ Intralox-EMS",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
